c42e99623367253bda4cd62babc71f2bff7c460c
Giordano Dìaz <diazgiordano@gmail.com>
2021-05-16 12:58:56 -0500

    Add devise gem and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    confirmations: 'confirmations'
+  }
+  resources :dashboard, only: [:index]
+  resources :home, only: [:index]
+  root "home#index"
 end
